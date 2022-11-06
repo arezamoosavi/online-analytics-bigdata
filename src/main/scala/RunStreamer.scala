87f4e896a8a62e6b5994a9c8d9e5d57cbcdae488
Alireza <sdamoosavi@gmail.com>
2022-11-06 15:34:59 +0100

    scala data to kafka added

---
+++ src/main/scala/RunStreamer.scala
@@ -0,0 +1,36 @@
+import org.apache.spark.sql.functions.{col, expr}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+// To see less warnings
+import org.apache.log4j._
+
+object RunStreamer extends App{
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val spark = SparkSession.builder()
+      .appName("Stream App")
+      .config("spark.master", "local")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    var raw_df = spark.read
+      .format("csv")
+      .option("sep", "\t")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .load("src/main/resources/user-ct-test-collection.txt")
+
+
+
+    raw_df.selectExpr("CAST(AnonID AS STRING) AS key", "to_json(struct(*)) AS value")
+      .write
+      .format("kafka")
+      .mode("append")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("topic", "click_topic")
+      .save()
+
+    println("Spark job finished!")
+    spark.stop()
+}
